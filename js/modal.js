b0b9577814dd0d1acf2adc00de09cd0db2cd110f
Axel <axel949@hotmail.com>
2019-05-22 13:08:12 +0200

    Started with tutorial

--- js/modal.js
+++ js/modal.js
@@ -4,12 +4,20 @@ var modal = document.getElementById("myModal");
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
 
+var next = document.getElementById("next");
+
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
+
+var hide = document.getElementById("hide");
+
+var bodyDiv = document.getElementById("bodyDiv");
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
+  hide.style.display = "block";
+  bodyDiv.style.background = "white";
 }
 
 // When the user clicks on <span> (x), close the modal
@@ -22,4 +30,8 @@ window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
+}
+
+function goNext() {
+  console.log("asd");
 }
